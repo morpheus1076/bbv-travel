17e72ec4f351c3c080dc33f30df442e4c9ba71fc
morpheus1076 <morpheus1076@gmailcom>
2024-10-28 04:11:06 +0100

    update

---
+++ Lang.lua
@@ -0,0 +1,6 @@
+
+
+Lang = {
+    GoToCayo = 'Drücke [E] um nach Cayo Perico zu fliegen.',
+    GoToLs = 'Drücke [E] um nach Los Santos zu fliegen.',
+}
---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.Teleports = {
+    CayoPerico = {
+        Teleport = vector3(4448.6665, -4479.2549, 4.2941),
+        Spawn = vector4(4461.5400, -4477.8696, 4.2329, 207.2478),
+        Blip = {
+            Sprite = 307,
+            Display = 4,
+            Colour = 2,
+            Size = 0.5,
+            Label = 'Cayo Perico - Flughafen'
+        }
+    },
+    LosSantos = {
+        Teleport = vector3(-1044.7555, -2749.6953, 21.3634),
+        Spawn = vector4(-1034.3593, -2750.1680, 14.5978, 334.7458),
+        Blip = {
+            Sprite = 307,
+            Display = 4,
+            Colour = 2,
+            Size = 0.5,
+            Label = 'Los Santos - Flughafen'
+        }
+    },
+}
